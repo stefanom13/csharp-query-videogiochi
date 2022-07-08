c25833b6383ab058dd64ca28367ad329fc53cb66
stefanom13 <stefanomancuso13@gmail.com>
2022-07-08 17:06:41 +0200

    test git push

--- query.sql
+++ query.sql
@@ -122,33 +122,55 @@
 -- 
 -- ------ Query con join
 -- 
--- ```
+
 -- 1- Selezionare i dati di tutti giocatori che hanno scritto almeno una recensione, mostrandoli una sola volta (996)
--- 
+
+
+
 -- 2- Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
--- 
+
+
+
 -- 3- Mostrare le categorie di ogni videogioco
--- SELECT v.id AS videogame_id, v.name AS videogame_name, v.release_date, c.id AS category_id, c.name AS category_name (1718)
--- 
+
+--SELECT v.id AS videogame_id, v.name AS videogame_name, v.release_date, c.id AS category_id, c.name AS category_name (1718)
+
 -- 4- Selezionare i dati di tutte le software house che hanno rilasciato almeno un gioco dopo il 2020, mostrandoli una sola volta (6)
--- 
+
+
+
 -- 5- Selezionare i premi ricevuti da ogni software house per i videogiochi che ha prodotto (55)
--- 
+
+
+
 -- 6- Selezionare categorie e classificazioni PEGI dei videogiochi che hanno ricevuto recensioni da 4 e 5 stelle, mostrandole una sola volta (3363)
--- 
+
+
+
 -- 7- Selezionare quali giochi erano presenti nei tornei nei quali hanno partecipato i giocatori il cui nome inizia per 'S' (474)
--- 
+
+
 -- 8- Selezionare le città in cui è stato giocato il gioco dell'anno del 2018 (36)
--- 
+
+
 -- 9- Selezionare i giocatori che hanno giocato al gioco più atteso del 2018 in un torneo del 2019 (3306)
--- 
+
+
+
 -- *********** BONUS ***********
 -- 
 -- 10- Selezionare i dati della prima software house che ha rilasciato un gioco, assieme ai dati del gioco stesso (software house id : 5)
--- 
+
+
+
 -- 11- Selezionare i dati del videogame (id, name, release_date, totale recensioni) con più recensioni (videogame id : 398)
--- 
+
+
+
 -- 12- Selezionare la software house che ha vinto più premi tra il 2015 e il 2016 (software house id : 1)
--- 
+
+
+
 -- 13- Selezionare le categorie dei videogame i quali hanno una media recensioni inferiore a 1.5 (10)
--- ```
+
+

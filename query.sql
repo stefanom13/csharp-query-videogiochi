aad8775457762224f9e905d4e367770910848df9
stefanom13 <stefanomancuso13@gmail.com>
2022-07-08 15:35:51 +0200

    groupBy completata

--- query.sql
+++ query.sql
@@ -69,10 +69,10 @@
 -- 
 -- 11- Selezionare il numero e la media delle recensioni per il videogioco con ID = 412 (review number = 12, avg_rating = 3)
 
-	--SELECT COUNT(*) AS 'review_number', AVG(avg_rating) AS 'Media'
+	--SELECT COUNT(*) AS 'review_number', AVG(rating) AS 'Media'
 	--FROM reviews
 	--WHERE videogame_id = '412';
-	--(non funzionante)
+
  
 -- 12- Selezionare il numero di videogame che la software house con ID = 1 ha rilasciato nel 2018 (13)
 
@@ -86,17 +86,39 @@
 -- 
 -- ```
 -- 1- Contare quante software house ci sono per ogni paese (3)
--- 
+
+	--SELECT country, COUNT(*) as SoftwareHousexCountry
+	--FROM software_houses
+	--GROUP BY country;
+
 -- 2- Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)
--- 
+
+	--SELECT videogame_id, COUNT(*) as Recensioni
+	--FROM reviews
+	--GROUP BY videogame_id;
+
 -- 3- Contare quanti videogiochi hanno ciascuna classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)
--- 
+
+	--SELECT pegi_label_id, COUNT(*) as NumeriVideoGames
+	--FROM pegi_label_videogame
+	--GROUP BY pegi_label_id;
+
 -- 4- Mostrare il numero di videogiochi rilasciati ogni anno (11)
--- 
+
+--SELECT YEAR(release_date) AS 'year', COUNT(*) as 'ReleasedGame'
+--FROM videogames
+--GROUP BY YEAR(release_date);
+
 -- 5- Contare quanti videogiochi sono disponbiili per ciascun device (del device vogliamo solo l'ID) (7)
--- 
+
+--SELECT device_id, COUNT(*) AS 'numeroVIdeogames'
+--FROM device_videogame
+--GROUP BY device_id;
+
 -- 6- Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)
--- ```
+
+--
+
 -- 
 -- ------ Query con join
 -- 
